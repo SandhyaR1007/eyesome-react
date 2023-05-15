8992d89c959f925cd3b5f8d0ff3fae5cc3145b01
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-16 02:27:09 +0530

    style: add custom classes for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./custom.styles.css";
 import { Navbar } from "./components";
-import { Home, ProductListing } from "./pages";
+import { Home, ProductDetails, ProductListing } from "./pages";
 
 const App = () => {
   return (
     <div className="mx-[5%] md:mx-[10%]">
       <Navbar />
       {/* <Home /> */}
-      <ProductListing />
+      {/* <ProductListing /> */}
+      <ProductDetails />
     </div>
   );
 };
