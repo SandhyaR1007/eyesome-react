140e960760fb9632bc1e61f0cd3e399686e5372a
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-17 23:34:01 +0530

    style: fix padding and margin for signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,12 +2,12 @@ import bannerHero from "../assets/bannerHero.jpg";
 
 const Signup = () => {
   return (
-    <main className="grid  grid-rows-1 lg:grid-cols-2 w-full md:w-2/3  lg:w-full h-screen m-auto">
+    <main className="grid  grid-rows-1 lg:grid-cols-2 w-full  h-screen m-auto">
       <section className=" hidden lg:block max-h-screen  rounded-lg">
         <img src={bannerHero} alt="" className="w-full h-full object-cover" />
       </section>
-      <div className="py-10 px-24 flex items-center justify-center w-full">
-        <section className="px-7 py-10 rounded-md shadow-md bg-white/[0.7] flex flex-col gap-6 w-full">
+      <div className="flex items-center justify-center w-full ">
+        <section className="px-7 py-10 rounded-md shadow-md bg-white/[0.7] flex flex-col gap-6 w-full max-w-lg">
           <div className="font-monoton text-3xl hover:text-red-600 cursor-pointer text-center">
             eyesome
           </div>
@@ -39,8 +39,8 @@ const Signup = () => {
             </form>
           </div>
 
-          <div className="w-full py-2  bg-black text-white flex flex-col gap-4 items-center">
-            <button className="btn-rounded-secondary flex items-center gap-2 md:text-sm lg:text-base">
+          <div className="w-full py-2   flex flex-col gap-4 items-center">
+            <button className="btn-rounded-secondary bg-black text-white flex items-center gap-2 md:text-sm lg:text-base">
               Sign up
             </button>
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from "./Home";
 export { default as ProductListing } from "./ProductListing";
+export { default as Signup } from "./Signup";
