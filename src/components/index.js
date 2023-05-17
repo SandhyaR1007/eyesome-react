af3b00ec66cdec831c96348be3af19e96f0f5698
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-18 00:21:28 +0530

    add: Login Page with styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./navbar/Navbar";
+export { default as Logo } from "./navbar/Logo";
 export { default as Banner } from "./banner/Banner";
 export { default as Trending } from "./trending/TrendingList";
 export { default as SingleProduct } from "./products/SingleProduct";
