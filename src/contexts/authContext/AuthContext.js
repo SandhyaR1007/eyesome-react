ab4780d57e216baebad21a04500ed3edfe237535
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-19 01:59:54 +0530

    update: login function with loading state

--- src/contexts/authContext/AuthContext.js
+++ src/contexts/authContext/AuthContext.js
@@ -1,9 +1,32 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router";
 
 const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem("authenticated")
+  );
+  const [loggingIn, setLoggingIn] = useState(false);
+
+  const login = ({ email, password }) => {
+    setLoggingIn(true);
+    if (email && password) {
+      localStorage.setItem("authenticated", true);
+      setIsAuthenticated(true);
+      setTimeout(() => {
+        setLoggingIn(false);
+        navigate("/");
+      }, 1000);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, loggingIn, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuthContext = () => useContext(AuthContext);
