4e775ea807e2cee4a117091d86cb4b932f943720
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-18 21:04:00 +0530

    update: add padding to form container, change button text

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,36 +3,36 @@ import { Logo } from "../components";
 
 const Signup = () => {
   return (
-    <main className="grid  grid-rows-1 lg:grid-cols-2 w-full  h-screen m-auto">
-      <section className=" hidden lg:block max-h-screen  rounded-lg">
+    <main className="grid  grid-rows-1 md:grid-cols-2 w-full  h-screen m-auto ">
+      <section className=" hidden md:block max-h-screen  rounded-lg">
         <img src={bannerHero} alt="" className="w-full h-full object-cover" />
       </section>
       <div className="flex items-center justify-center w-full px-5">
-        <section className="px-7 py-10 rounded-md shadow-md bg-white/[0.7] flex flex-col gap-6 w-full max-w-lg">
+        <section className="px-10 py-10 rounded-md shadow-md bg-white/[0.7] flex flex-col gap-6 w-full max-w-lg">
           <Logo />
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 ">
             <h1 className="text-4xl font-bold mb-3">Sign up</h1>
 
-            <form action="" className="flex flex-col gap-3">
+            <form action="" className="flex flex-col gap-5 py-5">
               <label className="flex flex-col">
-                Username
                 <input
                   type="text"
                   className="border rounded-md p-1.5 shadow-sm"
+                  placeholder="Username"
                 />
               </label>
               <label className="flex flex-col">
-                Email
                 <input
                   type="email"
                   className="border rounded-md p-1.5 shadow-sm"
+                  placeholder="Email"
                 />
               </label>
               <label className="flex flex-col">
-                Password
                 <input
                   type="password"
                   className="border rounded-md p-1.5 shadow-sm"
+                  placeholder="Password"
                 />
               </label>
             </form>
@@ -40,7 +40,7 @@ const Signup = () => {
 
           <div className="w-full py-2   flex flex-col gap-4 items-center">
             <button className="btn-primary w-2/3 text-lg text-center">
-              Create
+              Create Account
             </button>
             <p className="text-gray-600 text-sm">
               Already have an account?{" "}
