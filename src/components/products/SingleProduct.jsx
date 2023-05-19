653e54f9a0c9699254721cbca84851d93abd39dd
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-19 23:34:49 +0530

    style: change flex layout for card, height to image

--- src/components/products/SingleProduct.jsx
+++ src/components/products/SingleProduct.jsx
@@ -5,32 +5,38 @@ const SingleProduct = ({ product }) => {
   return (
     <div
       key={product.id}
-      className="flex flex-col justify-between bg-white/[0.5] rounded-lg shadow-md border-2 border-black/[0.05] overflow-hidden"
+      className="flex flex-col  bg-white/[0.5] rounded-lg shadow-md border-2 border-black/[0.05] overflow-hidden"
     >
-      <div className="p-10 bg-black/[0.075]">
-        <img src={product.image} alt="" className="w-full" />
+      <div className="flex items-center justify-center p-10 bg-black/[0.075] h-1/2">
+        <img
+          src={product.image}
+          alt=""
+          className="w-full h-full object-cover"
+        />
       </div>
 
-      <div className="p-3 flex flex-col gap-2 mt-2">
-        <p className=" flex justify-between">
-          <div className="flex flex-col">
-            <span className="text-xl font-medium">{product.name}</span>
-            <span className="flex items-center gap-1">
-              <span>{product.rating}</span>
+      <div className="p-3 flex flex-col justify-between gap-2 mt-2 h-1/2">
+        <div>
+          <div className=" flex justify-between">
+            <div className="flex flex-col">
+              <span className="text-xl font-medium">{product.name}</span>
+              <span className="flex items-center gap-1">
+                <span>{product.rating}</span>
 
-              <GiRoundStar className=" text-yellow-400 mb-1" />
-              <span className="text-xs text-gray-400">Rating</span>
-            </span>
-          </div>
+                <GiRoundStar className=" text-yellow-400 mb-1" />
+                <span className="text-xs text-gray-400">Rating</span>
+              </span>
+            </div>
 
-          <div className="flex flex-col items-end">
-            <span className="text-amber-600">₹{product.price}</span>
-            <span className="text-sm text-gray-600 line-through">
-              {product.price + 100}
-            </span>
+            <div className="flex flex-col items-end">
+              <span className="text-amber-600">${product.price}</span>
+              <span className="text-sm text-gray-600 line-through">
+                {product.price + 100}
+              </span>
+            </div>
           </div>
-        </p>
-        <p className="text-sm text-gray-600">{product.description}</p>
+          <p className="text-sm text-gray-600">{product.brand}</p>
+        </div>
         <div className="w-full pt-2 border-t flex justify-between items-center">
           <button className="border border-[--primary-text-color]  py-1.5 text-sm  rounded-full px-6 hover:bg-[--primary-text-color] hover:text-white transition">
             Add to Cart
