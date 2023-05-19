2b8c74afdea9a2ed726c1e6cf067c132b0a1493f
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-20 00:41:44 +0530

    fix: replace login url to signup url for signup service

--- src/api/apiServices.js
+++ src/api/apiServices.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { GET_ALL_PRODUCTS_URL, LOGIN_URL } from "./apiUrls";
+import { GET_ALL_PRODUCTS_URL, LOGIN_URL, SIGNUP_URL } from "./apiUrls";
 
 export const loginService = (email, password) =>
   axios.post(LOGIN_URL, { email, password });
 
 export const signupService = (username, email, password) =>
-  axios.post(LOGIN_URL, { username, email, password });
+  axios.post(SIGNUP_URL, { username, email, password });
 
 export const getAllProductsService = () => axios.get(GET_ALL_PRODUCTS_URL);
