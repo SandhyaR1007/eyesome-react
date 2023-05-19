1177dd84e8d5f8e419899b1fd6e0f2f56a3a31b0
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-19 22:52:59 +0530

    update: added loading state, getAllProducts api call

--- src/contexts/productsContext/ProductsContext.js
+++ src/contexts/productsContext/ProductsContext.js
@@ -1,9 +1,48 @@
-import { createContext, useContext } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { initialState, productsReducer } from "../../reducers/productsReducer";
+import { getAllProductsService } from "../../api/apiServices";
+import { actionTypes } from "../../utils/actionTypes";
 
 export const ProductsContext = createContext();
 
 const ProductsContextProvider = ({ children }) => {
-  return <ProductsContext.Provider>{children}</ProductsContext.Provider>;
+  const [loading, setLoading] = useState(false);
+  const [state, dispatch] = useReducer(productsReducer, initialState);
+
+  useEffect(async () => {
+    setLoading(true);
+    try {
+      const response = await getAllProductsService();
+      if (response.status === 200) {
+        dispatch({
+          type: actionTypes.INITIALIZE_PRODUCTS,
+          payload: response.data.products,
+        });
+      }
+      console.log({ response });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return (
+    <ProductsContext.Provider
+      value={{
+        allProducts: state.allProducts,
+        loading,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
 };
 
 export default ProductsContextProvider;
