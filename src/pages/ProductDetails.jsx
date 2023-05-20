4c4f1ea8548ed02eb30e26d04de4578c6ab36cb7
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-21 00:32:19 +0530

    update: made product details dynamic with details from products array

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,39 +1,51 @@
-import glassCategory1 from "../assets/glassCategory1.png";
-
-import { GiRoundStar } from "react-icons/gi";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { HiOutlineShoppingBag } from "react-icons/hi";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { BsBookmarkHeart } from "react-icons/bs";
 
+import { useAuthContext, useProductsContext } from "../contexts";
+import { getProductByIdService } from "../api/apiServices";
+import { StarRating } from "../components";
+
 const ProductDetails = () => {
-  const product = {
-    id: 1,
-    name: "Classic Aviator",
-    price: 4999.99,
-    category: "sunglasses",
-    rating: 4.5,
-    image: glassCategory1,
-    description:
-      "These classic aviator sunglasses are perfect for any occasion.",
-  };
+  const navigate = useNavigate();
+  const { productId } = useParams();
+  const { isAuthenticated } = useAuthContext();
+  const { getProductById, addProductToCart, allProducts } =
+    useProductsContext();
+  const [loading, setLoading] = useState(false);
+  const product = getProductById(productId);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const response = await getProductByIdService(productId);
+        product = response?.data?.product;
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [allProducts]);
+
   return (
     <div className="md:min-h-[80vh] flex justify-center items-center py-3">
       <main className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-10 ">
         <section className="p-10 bg-black/[0.075] max-h-screen flex items-center rounded-lg">
-          <img src={glassCategory1} alt="" className="w-full " />
+          <img src={product?.image} alt="" className="w-full " />
         </section>
 
         <section className="p-7 px-10 rounded-md shadow-sm bg-white/[0.7] flex flex-col gap-6 ">
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-bold">{product.name}</h1>
-            <p className=" text-gray-600 text-lg">{product.description}</p>
+            <h1 className="text-4xl font-bold">{product?.name}</h1>
+            <p className=" text-gray-600 text-lg">{product?.description}</p>
             <div className="flex items-center gap-1">
-              {new Array(5).fill().map((i) => (
-                <GiRoundStar className=" text-yellow-400 mb-1" />
-              ))}
+              <StarRating />
 
               <span className="text-xs text-gray-400">
-                ({product.rating}) Rating
+                ({product?.rating}) Rating
               </span>
             </div>
           </div>
@@ -42,41 +54,51 @@ const ProductDetails = () => {
             <h2 className="  text-lg">About Product</h2>
             <ul>
               <li>
-                <span className="text-gray-500 text-sm">Brand: </span>Some Brand
+                <span className="text-gray-500 text-sm">Brand: </span>
+                {product?.brand}
               </li>
               <li>
                 <span className="text-gray-500 text-sm">Category: </span>
-                {product.category}
+                {product?.category}
               </li>
               <li>
-                <span className="text-gray-500">Heavy: </span>200g
+                <span className="text-gray-500 text-sm">Gender: </span>
+                {product?.gender}
+              </li>
+              <li>
+                <span className="text-gray-500">Heavy: </span>
+                {product?.weight}
               </li>
             </ul>
           </div>
 
-          <div className="flex flex-col ">
-            <span className="text-2xl text-amber-600">₹{product.price}</span>
-            <span className="text-sm text-gray-600 line-through">
-              {product.price + 100}
-            </span>
-          </div>
           <div className="flex gap-2 items-center">
-            <button className="btn-primary text-lg">
-              <AiOutlineMinus />
-            </button>
-            <span className="h-full w-14 bg-black/[0.075]  rounded-sm flex items-center justify-center">
-              0
+            Price:
+            <span className="ms-1 text-2xl text-amber-600">
+              ₹{product?.price}
             </span>
-            <button className="btn-primary text-lg">
-              <AiOutlinePlus />
-            </button>
-            <div>
-              <span className="text-gray-500">Stock: </span>15
-            </div>
+            <span className="text-sm text-gray-600 line-through">
+              ₹{product?.price + 100}
+            </span>
           </div>
+
           <div className="w-full py-2   flex gap-4 items-center">
-            <button className="btn-rounded-secondary flex items-center gap-2 md:text-sm lg:text-base">
-              <HiOutlineShoppingBag /> Add to Bag
+            <button
+              className="btn-rounded-secondary flex items-center gap-2 md:text-sm lg:text-base"
+              onClick={() => {
+                if (!isAuthenticated) {
+                  navigate("/login");
+                } else {
+                  if (!product?.inCart) {
+                    addProductToCart(product);
+                  } else {
+                    navigate("/cart");
+                  }
+                }
+              }}
+            >
+              <HiOutlineShoppingBag />{" "}
+              {product?.inCart ? "Go to Bag" : "Add to Bag"}
             </button>
             <button className="btn-rounded-primary rounded-full flex items-center gap-2 md:text-sm lg:text-base">
               <BsBookmarkHeart /> <span>Wishlist Item</span>
