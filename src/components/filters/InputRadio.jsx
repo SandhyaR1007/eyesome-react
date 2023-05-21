ace41dc67ea3da16f07fea78b335f2ef685a8ede
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-21 16:23:45 +0530

    update: implemented filter by rating functionality

--- src/components/filters/InputRadio.jsx
+++ src/components/filters/InputRadio.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useProductsContext } from "../../contexts";
 
 const InputRadio = ({ data, name }) => {
+  const { applyFilters } = useProductsContext();
   return (
     <label>
-      <input type="radio" className="" name={name} /> {data} Stars & above
+      <input
+        type="radio"
+        className=""
+        name={name}
+        value={data}
+        onChange={(e) => applyFilters(name, data)}
+      />{" "}
+      {data} Stars & above
     </label>
   );
 };
