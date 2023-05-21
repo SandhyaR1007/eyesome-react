5cdbc21b1c4a79c55144dde88469421e44f2973c
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-22 01:00:53 +0530

    add: CategoryList and CategoryCard

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { default as Navbar } from "./navbar/Navbar";
 export { default as Logo } from "./navbar/Logo";
 export { default as Banner } from "./banner/Banner";
 export { default as Trending } from "./trending/TrendingList";
+export { default as CategoryList } from "./categories/CategoryList";
 
 export { default as SingleProduct } from "./products/SingleProduct";
 export { default as StarRating } from "./products/StarRating";
