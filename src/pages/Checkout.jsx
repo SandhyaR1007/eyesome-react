9f8ed129ede3bbabf86d70e5b1208ca79a70b2c9
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-22 23:07:28 +0530

    update: dynamic address form and card

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,95 +1,31 @@
-import { useState } from "react";
-import { SummaryCard } from "../components";
+import { AddressCard, AddressForm, SummaryCard } from "../components";
+import { useProductsContext } from "../contexts";
 
 const Checkout = () => {
-  const [addressData, setAddressData] = useState([
-    {
-      address: "8505 Christina Ridges West Cooper Arunachal Pradesh Pin:820598",
-      mobile: 1293452481,
-    },
-  ]);
-  const [newAddress, setNewAddress] = useState({
-    flat: "aaaa54665464",
-    area: "ccc y556567788",
-    town: "aaa y698590809",
-    pincode: "123345",
-  });
+  const [showAddressForm, setShowAddressForm] = useState(false);
+  const { addressList } = useProductsContext();
 
-  const AddressCard = ({ address, pincode }) => {
-    return (
-      <label className="flex bg-gray-50 items-center gap-2 shadow-sm p-4 rounded-sm">
-        <input type="radio" name="address" id="" className="accent-current" />
-        <p>
-          {address}
-          Mobile:
-          {pincode}
-        </p>
-      </label>
-    );
-  };
   return (
     <div className="md:min-h-[80vh] flex justify-center items-center py-3">
       <main className="grid md:grid-cols-2 gap-10 w-full">
         <section className="p-7 rounded-md shadow-sm bg-white/[0.7] flex flex-col gap-6 w-full h-min">
           <h1 className="text-3xl font-bold">Address</h1>
-          {addressData.map(({ address, pincode }) => (
-            <AddressCard address={address} pincode={pincode} />
+          {addressList.map((address) => (
+            <AddressCard address={address} />
           ))}
-          <h3 className="text-denter text-gray-600">OR</h3>
-          <form
-            action=""
-            className="flex flex-col gap-3 p-5 bg-gray-50 shadow-sm"
-          >
-            <label className="flex flex-col">
-              Flat, House no., Building
-              <input
-                type="text"
-                className="border rounded-md p-1.5 shadow-sm"
-              />
-            </label>
-            <label className="flex flex-col">
-              Area, Colony, Street
-              <input
-                type="email"
-                className="border rounded-md p-1.5 shadow-sm"
-              />
-            </label>
-            <div className="flex gap-2 flex-wrap">
-              <label className="flex flex-1 flex-col">
-                Town/City
-                <input
-                  type="password"
-                  className="border rounded-md p-1.5 shadow-sm"
-                />
-              </label>
-              <label className="flex flex-1 flex-col">
-                Pin Code
-                <input
-                  type="password"
-                  className="border rounded-md p-1.5 shadow-sm"
-                />
-              </label>
-            </div>
-            <div className="flex gap-3 mt-3">
-              <button className="btn-rounded-secondary rounded-full flex items-center gap-2 md:text-sm lg:text-base">
-                Cancel
-              </button>
+          {showAddressForm ? (
+            <AddressForm setShowAddressForm={setShowAddressForm} />
+          ) : (
+            <div className="flex flex-col items-start">
+              <h3 className=" text-gray-600 ms-1 my-2">OR</h3>
               <button
-                className="btn-rounded-primary rounded-full flex items-center gap-2 md:text-sm lg:text-base"
-                onClick={() => {
-                  setAddressData([
-                    ...addressData,
-                    {
-                      address: `${newAddress.area},${newAddress.flat},${newAddress.town}`,
-                      pincode: newAddress.pincode,
-                    },
-                  ]);
-                }}
+                className="btn-rounded-primary text-sm"
+                onClick={() => setShowAddressForm(!showAddressForm)}
               >
-                Save
+                + Add New Address
               </button>
             </div>
-          </form>
+          )}
         </section>
         <SummaryCard />
       </main>
