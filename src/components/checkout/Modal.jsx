1cec377661604cff2c1eec3cffb716b9616ef276
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-24 19:02:03 +0530

    add: Modal component

---
+++ src/components/checkout/Modal.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import spinningLoader from "../../assets/spinning-circles.svg";
+import OrderSummary from "./OrderSummary";
+
+const Modal = ({ showModal, setShowModal, setIsOrderPlaced }) => {
+  const [disableBtn, setDisableBtn] = useState(false);
+  const clickHandler = () => {
+    setDisableBtn(true);
+    setTimeout(() => {
+      setIsOrderPlaced(true);
+      setShowModal(false);
+      setDisableBtn(false);
+    }, 2000);
+  };
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div className="transition justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-xl font-semibold">Order Summary</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <OrderSummary />
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    disabled={disableBtn}
+                    className="btn-rounded-primary w-1/2  text-sm ease-linear transition-all duration-150
+                    disabled:cursor-wait
+                    "
+                    type="button"
+                    onClick={clickHandler}
+                  >
+                    {disableBtn ? (
+                      <img src={spinningLoader} alt="" height={20} />
+                    ) : (
+                      <span>Confirm Order</span>
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default Modal;
