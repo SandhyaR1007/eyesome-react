4ed04f11b151705c43fb94e2fa3c341786cba979
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-27 00:32:50 +0530

    style: update text color

--- src/components/navbar/MenuDropdown.jsx
+++ src/components/navbar/MenuDropdown.jsx
@@ -4,7 +4,7 @@ import { HiOutlineShoppingBag } from "react-icons/hi";
 const MenuDropdown = ({ navigate }) => {
   return (
     <div className="absolute right-0 z-10  bg-amber-50 font-semibold   rounded-lg shadow w-max  overflow-hidden transition-all">
-      <ul className="text-sm text-current dark:text-gray-200">
+      <ul className="text-sm  ">
         <li onClick={() => navigate("/wishlist")}>
           <span className="flex items-center px-5 py-3 hover:bg-amber-100 ">
             <BsBookmarkHeart className="text-lg me-3" /> Wishlist
