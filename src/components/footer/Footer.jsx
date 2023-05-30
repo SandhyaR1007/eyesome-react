d49764e6c51334172f7351ecfee1cf48b82f34a0
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-31 01:21:21 +0530

    update: added positioning, social media links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import {
+  AiOutlineLinkedin,
+  AiFillGithub,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className="py-10 mt-3 w-full bg-amber-50 text-center">
-      Eyesome made with 💜 by Sandhya{" "}
+    <div className="py-5 mt-3  bg-amber-50 flex flex-col justify-center items-center gap-2 absolute right-0 left-0">
+      <p>Eyesome made with 💜 by Sandhya </p>
+      <p className="flex gap-3">
+        <a href="https://github.com/SandhyaR1007">
+          <AiFillGithub className="text-2xl text-gray-800" />
+        </a>
+        <a href="/">
+          {" "}
+          <AiOutlineLinkedin className="text-2xl text-gray-800" />
+        </a>
+        <a href="/https://twitter.com/SandhyaR1007">
+          <AiOutlineTwitter className="text-2xl text-gray-800" />
+        </a>
+      </p>
     </div>
   );
 };
