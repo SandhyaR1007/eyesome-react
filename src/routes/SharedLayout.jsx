b3af2b9a7539d6a1f79d311893c44e20d933a267
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-01 01:47:19 +0530

    add: Loader component

--- src/routes/SharedLayout.jsx
+++ src/routes/SharedLayout.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router";
-import { Navbar } from "../components";
+import { Loader, Navbar } from "../components";
 
 const SharedLayout = () => {
+  const [loadingData, setLoadingData] = useState(false);
+  useEffect(() => {
+    setLoadingData(true);
+    const id = setTimeout(() => {
+      setLoadingData(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
+
   return (
     <div className="px-[4%] md:px-[10%] pb-2">
       <Navbar />
-      <div className="pt-32 sm:pt-20">
-        <Outlet />
+      <div className="pt-32 sm:pt-20 min-h-[80vh]">
+        {loadingData ? <Loader /> : <Outlet />}
       </div>
     </div>
   );
